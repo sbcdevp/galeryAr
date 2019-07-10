dca90ba6fa5d95ffd7736063e93ebef99a141c26
Sergiu_Bo <bocanceaserge335566@gmail.com>
2019-07-10 18:17:14 +0200

    add cursor background

--- src/javascript/components/Gallery.js
+++ src/javascript/components/Gallery.js
@@ -5,9 +5,12 @@ window.gallery.images = {
         'use strict';
         this.container = document.querySelector('.js-container');
         this.navigation = this.container.querySelector('.js-navigation');
-        this.cursor= this.navigation.querySelector('.js-cursor');
+        this.leftContainer = this.navigation.querySelector('.js-left');
+        this.rightContainer = this.navigation.querySelector('.js-right');
+        this.cursor = this.navigation.querySelector('.js-cursor');
         console.log(this.cursor)
         this.container.addEventListener('mousemove', this.cursorMove.bind(this));
+        this.initListeners();
 
     },
     cursorMove: function (event) {
@@ -18,10 +21,33 @@ window.gallery.images = {
         this.animation.add(
         TweenMax.to(this.cursor, 0.5, {x: this.mouseX, y: this.mouseY}));
     },
+    initListeners: function () {
+        'use strict';
+        this.leftContainer.addEventListener('mouseenter', this.displayImages.bind(this, false));
+        this.leftContainer.addEventListener('click', this.changeImage.bind(this, false));
+        this.rightContainer.addEventListener('mouseenter', this.displayImages.bind(this, true));
+        this.rightContainer.addEventListener('click', this.changeImage.bind(this, true));
+    },
+    displayImages: function (sens) {
+        'use strict';
+        event.preventDefault();
+        var i = 2;
+        if (sens === true) {
+         i += 1
+        } else {
+         i -= 1
+        }
+        this.cursor.style.backgroundImage = "url('../images/img_"+ i + ".jpg')";
+        console.log(this.cursor)
+    },
+    changeImage: function (sens) {
+        'use strict';
+        console.log("clické", sens)
+    },
     invoke: function () {
         'use strict';
         return {
-            init: this.init.bind(this),
+            init: this.init.bind(this)
         };
     }
 }.invoke();
