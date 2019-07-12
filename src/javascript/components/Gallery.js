5efa07dae1674ec6bbf76b0a6e5d3596fd8acf53
Sergiu_Bo <bocanceaserge335566@gmail.com>
2019-07-12 11:21:02 +0200

    add name

--- src/javascript/components/Gallery.js
+++ src/javascript/components/Gallery.js
@@ -5,11 +5,15 @@ window.gallery.images = {
         'use strict';
         this.container = document.querySelector('.js-container');
         this.navigation = this.container.querySelector('.js-navigation');
+        this.nameContainer = this.container.querySelector('.js-names');
         this.leftContainer = this.navigation.querySelector('.js-left');
         this.rightContainer = this.navigation.querySelector('.js-right');
-        this.cursor = this.navigation.querySelector('.js-cursor');
-        console.log(this.cursor)
-        this.container.addEventListener('mousemove', this.cursorMove.bind(this));
+        this.cursor = this.container.querySelector('.js-cursor');
+        this.images = this.container.querySelectorAll('.container-images img');
+        this.imageName = this.container.querySelector('.js-image-name')
+        this.indexLength = this.images.length;
+        this.index = 0;
+        this.images[0].classList.add('active');
         this.initListeners();
 
     },
@@ -19,30 +23,75 @@ window.gallery.images = {
         this.mouseY = event.pageY;
         this.animation = new TimelineMax();
         this.animation.add(
-        TweenMax.to(this.cursor, 0.5, {x: this.mouseX, y: this.mouseY}));
+        TweenMax.to(this.cursor, 1, {x: this.mouseX, y: this.mouseY}));
     },
     initListeners: function () {
         'use strict';
-        this.leftContainer.addEventListener('mouseenter', this.displayImages.bind(this, false));
-        this.leftContainer.addEventListener('click', this.changeImage.bind(this, false));
-        this.rightContainer.addEventListener('mouseenter', this.displayImages.bind(this, true));
-        this.rightContainer.addEventListener('click', this.changeImage.bind(this, true));
+        this.container.addEventListener('mousemove', this.cursorMove.bind(this));
+        this.nameContainer.addEventListener('mouseenter', this.nameHover.bind(this));
+        this.nameContainer.addEventListener('mouseleave', this.nameHover.bind(this));
+        this.leftContainer.addEventListener('mousemove', this.nextCursor.bind(this, false));
+        this.leftContainer.addEventListener('click', this.displayImages.bind(this, false));
+        this.rightContainer.addEventListener('mousemove', this.nextCursor.bind(this, true));
+        this.rightContainer.addEventListener('click', this.displayImages.bind(this, true));
+        this.clicked = false;
+    },
+    nameHover: function (event) {
+        'use strict';
+        if (event.type === 'mouseenter') {
+            this.cursor.classList.add('hidden');
+            this.container.style.cursor = '';
+        } else {
+            this.cursor.classList.remove('hidden');
+        }
+
     },
     displayImages: function (sens) {
         'use strict';
-        event.preventDefault();
-        var i = 2;
-        if (sens === true) {
-         i += 1
-        } else {
-         i -= 1
+        this.newIndex = sens ? this.index + 1 : this.index - 1;
+        this.yMove = sens ? "20" : '-20'
+        if (this.newIndex < 0) {
+          this.newIndex = this.indexLength - 1;
         }
-        this.cursor.style.backgroundImage = "url('../images/img_"+ i + ".jpg')";
-        console.log(this.cursor)
+        if (this.newIndex === this.indexLength) {
+          this.newIndex = 0
+        }
+        this.images[this.newIndex].getAttribute('data-name');
+        // console.log(this.images[newIndex].getAttribute('data-name'));
+        var timeline = new TimelineMax();
+        if (this.clicked === false) {
+            this.clicked = true;
+            console.log('event fired')
+            this.animation.add(
+            TweenMax.to(this.imageName, 0.2, {y: this.yMove, opacity: 0}))
+            .add(
+            TweenMax.to(this.imageName, 0.1, {y: -this.yMove, opacity: 0, onComplete: this.changeName.bind(this, sens)}))
+            .add(
+            TweenMax.to(this.imageName, 0.5, {y: 0, opacity: 1}));
+        }
+
+        console.log('cfez');
     },
-    changeImage: function (sens) {
+    changeName: function (sens) {
         'use strict';
-        console.log("clické", sens)
+        this.clicked = false;
+        this.images[this.index].classList.remove("active");
+        this.images[this.newIndex].classList.add("active");
+        this.index = this.newIndex;
+        this.imageName.innerHTML = this.images[this.newIndex].getAttribute('data-name');
+        this.nextCursor(sens);
+    },
+    nextCursor: function (sens) {
+        'use strict';
+        var indexCursor = sens ? this.index + 1 : this.index - 1;
+        this.nextImage = indexCursor;
+        if (indexCursor < 0) {
+          this.nextImage = this.indexLength - 1;
+        }
+        if (indexCursor === this.indexLength) {
+          this.nextImage = 0;
+        }
+        this.cursor.style.backgroundImage = "url('../images/img_"+ this.nextImage + ".jpg')";
     },
     invoke: function () {
         'use strict';
