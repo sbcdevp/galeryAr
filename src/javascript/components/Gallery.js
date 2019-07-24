c6c16e79d9085a05e26fc5478ec6dc9b7585c944
Sergiu_Bo <bocanceaserge335566@gmail.com>
2019-07-24 14:31:35 +0200

    add mobile or pc detection

--- src/javascript/components/Gallery.js
+++ src/javascript/components/Gallery.js
@@ -15,9 +15,6 @@ window.gallery.images = {
     initGeometry: function () {
         'use strict';
         this.materials = [];
-        this.helperGeometry = new THREE.BoxBufferGeometry( 120, 120, 120, 1, 4, 4 );
-        this.helperMaterial = new THREE.MeshBasicMaterial( { color: 0xff00ff, wireframe: true } );
-        this.helper = new THREE.Mesh( this.helperGeometry, this.helperMaterial );
         this.createVerticalCubes();
         this.createHorizontalCubes();
         this.createFaceCubes();
@@ -31,9 +28,9 @@ window.gallery.images = {
             i;
         for (i = 0; i < 12; i++) {
             modulo = i % 2;
-            randomPosX = -60 + (i * 10);
-            randomPosZ = modulo === 0 ? 30 : -50;
-            randomPosY = Math.round(Math.random() * 80 - 40);
+            randomPosX = -50 + (i * 10);
+            randomPosZ = modulo === 0 ? 60 : -60;
+            randomPosY = Math.round(Math.random() * 120 - 60);
             this.materials.push(this.material = new THREE.MeshBasicMaterial( {
                         map: new THREE.TextureLoader().load( 'images/img_'+ i +'.jpg', this.displayVerticalImg.bind(this, i, randomPosX, randomPosY, randomPosZ, modulo) ),
                         side: THREE.DoubleSide
@@ -41,11 +38,11 @@ window.gallery.images = {
         }
     },
     displayVerticalImg: function(i, x, y, z, modulo) {
-        var imgWidth = this.materials[i].map.image.width / 42,
-            imgHeight = this.materials[i].map.image.height / 42,
+        var imgWidth = this.materials[i].map.image.width / 32,
+            imgHeight = this.materials[i].map.image.height / 32,
             geometry = new THREE.PlaneGeometry( imgWidth, imgHeight, 10 );
         this.verticalCube = new THREE.Mesh( geometry, this.materials[i] );
-        this.verticalCube.position.set( x, y, z + i * 3);
+        this.verticalCube.position.set( x, y, z + i * .5);
         if (modulo === 0) {
             this.verticalCube.rotation.y = 3.15;
             if (x > 45) {
@@ -62,7 +59,7 @@ window.gallery.images = {
             }
         }
         this.verticalCube.scale.set(0.8, 0.8, 0.8)
-        this.scene.add(this.verticalCube, this.helper);
+        this.scene.add(this.verticalCube);
     },
     createHorizontalCubes: function () {
         var modulo,
@@ -72,8 +69,8 @@ window.gallery.images = {
         for (var i = 12; i < 24; i++) {
             modulo = i % 2;
             randomPosX = -60 + (i * 10)- 120;
-            randomPosZ = Math.round(Math.random() * 120 - 60);
-            randomPosY = modulo === 0 ? 40 : -40;
+            randomPosZ = Math.round(Math.random() * 60 - 30);
+            randomPosY = modulo === 0 ? 55 : -70;
             this.materials.push(this.material = new THREE.MeshBasicMaterial( {
                         map: new THREE.TextureLoader().load( 'images/img_'+ i +'.jpg', this.displayHorizontalImg.bind(this, i, randomPosX, randomPosY, randomPosZ, modulo)),
                         side: THREE.DoubleSide
@@ -81,16 +78,16 @@ window.gallery.images = {
         }
     },
     displayHorizontalImg: function(i, x, y, z, modulo) {
-        var imgWidth = this.materials[i].map.image.width / 42,
-            imgHeight = this.materials[i].map.image.height / 42,
+        var imgWidth = this.materials[i].map.image.width / 32,
+            imgHeight = this.materials[i].map.image.height / 32,
             geometry = new THREE.PlaneGeometry( imgWidth, imgHeight, 10 );
         this.horizontalCube = new THREE.Mesh( geometry, this.materials[i] );
         this.horizontalCube.position.set( x, y + i / 4, z);
-        if (z < 0) {
+        if (z > 0) {
             this.horizontalCube.rotation.z = 59.7;
         }
         this.horizontalCube.rotation.x = 29.85;
-        this.horizontalCube.scale.set(0.8, 0.8, 0.8)
+        this.horizontalCube.scale.set(0.7, 0.7, 0.7)
         this.scene.add(this.horizontalCube);
     },
     createFaceCubes: function () {
@@ -101,7 +98,7 @@ window.gallery.images = {
         for (var i = 24; i < 36; i++) {
             modulo = i % 2;
             randomPosX = modulo === 0 ? 60 : -60;
-            randomPosZ = Math.round(Math.random() * 120 - 60);
+            randomPosZ = Math.round(Math.random() * 100 - 50);
             randomPosY = -60 + (i * 10) - 240;
             this.materials.push(this.material = new THREE.MeshBasicMaterial( {
                         map: new THREE.TextureLoader().load( 'images/img_'+ i +'.jpg', this.displayFaceImg.bind(this, i, randomPosX, randomPosY, randomPosZ) ),
@@ -110,8 +107,8 @@ window.gallery.images = {
         }
     },
     displayFaceImg: function(i, x, y, z) {
-        var imgWidth = this.materials[i].map.image.width / 42,
-            imgHeight = this.materials[i].map.image.height / 42;
+        var imgWidth = this.materials[i].map.image.width / 32,
+            imgHeight = this.materials[i].map.image.height / 32;
         var geometry = new THREE.PlaneGeometry( imgWidth, imgHeight, 10 );
         this.faceCube = new THREE.Mesh( geometry, this.materials[i] );
         this.faceCube.position.set( x, y, z);
@@ -124,12 +121,16 @@ window.gallery.images = {
     },
     render: function () {
         this.renderer = new THREE.WebGLRenderer( { antialias: true } );
-        this.controls = new OrbitControls( this.camera, this.renderer.domElement);
+        if (window.innerWidth > 560) {
+            this.controls = new OrbitControls( this.camera, this.renderer.domElement);
+            this.camera.position.set( 0, 20, 100 );
+            this.controls.update();
+        }else {
+            this.controls = new DeviceOrientationControls( this.camera );
+        }
         this.renderer.setPixelRatio( window.devicePixelRatio );
         this.renderer.setSize( window.innerWidth, window.innerHeight );
         this.container.appendChild( this.renderer.domElement );
-        this.camera.position.set( 0, 20, 100 );
-        this.controls.update();
     },
     animate: function () {
         window.requestAnimationFrame(this.animate.bind(this));
